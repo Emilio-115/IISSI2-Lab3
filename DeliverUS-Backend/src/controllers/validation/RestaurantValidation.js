0b4dd136a0ca0e95a835c65129442d758a72ea8c
Emilio-115 <emivazcru@alum.us.es>
2024-03-08 12:27:45 +0100

    Casi completo

--- DeliverUS-Backend/src/controllers/validation/RestaurantValidation.js
+++ DeliverUS-Backend/src/controllers/validation/RestaurantValidation.js
@@ -17,8 +17,13 @@ const create = [
   }).withMessage('Please upload an image with format (jpeg, png).'),
   check('logo').custom((value, { req }) => {
     return checkFileMaxSize(req, 'logo', maxFileSize)
-  }).withMessage('Maximum file size of ' + maxFileSize / 1000000 + 'MB')
-  // TODO: Complete validations
+  }).withMessage('Maximum file size of ' + maxFileSize / 1000000 + 'MB'),
+  check('address').exists().isString().isLength({ min: 1, max: 255 }).trim(),
+  check('postalCode').exists().isString().isLength({ min: 1, max: 255 }).trim(),
+  check('url').optional({ nullable: true, checkFalsy: true }).isString().trim(),
+  check('email').optional({ nullable: true, checkFalsy: true }).isString().trim(),
+  check('phone').optional({ nullable: true, checkFalsy: true }).isString().trim()
+  // Terminar
 ]
 const update = [
   check('name').exists().isString().isLength({ min: 1, max: 255 }).trim(),
@@ -36,7 +41,7 @@ const update = [
   check('logo').custom((value, { req }) => {
     return checkFileMaxSize(req, 'logo', maxFileSize)
   }).withMessage('Maximum file size of ' + maxFileSize / 1000000 + 'MB')
-  // TODO: Complete validations
+  //
 ]
 
 export { create, update }
